6f3ee452f012ec41787244d9f28a8e19efb329b4
Zilant1337 <grigorenko_p@inbox.ru>
2023-09-26 17:27:55 +0300

    Minor terminology and other changes

--- GraphsComputerNetwork/Graphs.cs
+++ GraphsComputerNetwork/Graphs.cs
@@ -16,35 +16,36 @@ namespace GraphsComputerNetwork
             private float dataPassthroughModifier;
             private int number;
 
-            public Vertex(float xCoordinate, float yCoordinate, float dataPassthroughModifier=1,string name="")
+            public Vertex(float xCoordinate, float yCoordinate, float dataPassthroughModifier = 1, string name = "", int number = -1)
             {
                 this.dataPassthroughModifier = dataPassthroughModifier;
                 this.xCoordinate = xCoordinate;
                 this.yCoordinate = yCoordinate;
                 this.name = name;
+                this.number = number;
             }
             public float GetXCoordinate()
             {
                 return xCoordinate;
             }
-            public float GetYCoordinate ()
+            public float GetYCoordinate()
             {
                 return yCoordinate;
             }
             public void SetXCoordinate(float xCoordinate)
             {
-                this.xCoordinate=xCoordinate;
+                this.xCoordinate = xCoordinate;
             }
             public void SetTCoordinate(float yCoordinate)
             {
-                this.yCoordinate=yCoordinate;
+                this.yCoordinate = yCoordinate;
             }
             public void SetCoordinates(int xCoordinate, int yCoordinate)
             {
                 this.xCoordinate = xCoordinate;
                 this.yCoordinate = yCoordinate;
             }
-            
+
             public string GetName()
             {
                 return name;
@@ -55,7 +56,7 @@ namespace GraphsComputerNetwork
             }
             public void SetDataPassthroughModifier(float dataPassthroughModifier)
             {
-                this.dataPassthroughModifier= dataPassthroughModifier;
+                this.dataPassthroughModifier = dataPassthroughModifier;
             }
             public float GetDataPassthroughModifier()
             {
@@ -67,28 +68,28 @@ namespace GraphsComputerNetwork
             }
             public void SetNumber(int number)
             {
-                this.number = number;  
+                this.number = number;
             }
             public double GetDistance(Vertex otherVertex)
             {
-                return Math.Sqrt(Math.Pow((otherVertex.GetXCoordinate()-this.GetXCoordinate()),2)+Math.Pow((otherVertex.GetYCoordinate() - this.GetYCoordinate()), 2));
+                return Math.Sqrt(Math.Pow((otherVertex.GetXCoordinate() - this.GetXCoordinate()), 2) + Math.Pow((otherVertex.GetYCoordinate() - this.GetYCoordinate()), 2));
             }
 
         }
-        public class Branch
+        public class Edge
         {
             private double length;
             private float modifier;
-            private int startVertexNumber;
-            private int endVertexNumber;
+            private Vertex startVertex;
+            private Vertex endVertex;
             private int number;
             private bool isDirected;
 
-            public Branch(bool isDirected, int startVertexNumber, int endVertexNumber, int number, double length = 0,float modifier=1)
+            public Edge(bool isDirected, Vertex startVertex, Vertex endVertex, int number, double length = 0, float modifier = 1)
             {
                 this.isDirected = isDirected;
-                this.startVertexNumber = startVertexNumber;
-                this.endVertexNumber = endVertexNumber;
+                this.startVertex = startVertex;
+                this.endVertex = endVertex;
                 this.number = number;
                 this.length = length;
             }
@@ -100,21 +101,21 @@ namespace GraphsComputerNetwork
             {
                 this.length = length;
             }
-            public int GetStartVertexNumber()
+            public Vertex GetStartVertex()
             {
-                return startVertexNumber;
+                return startVertex;
             }
-            public int GetEndVertexNumber()
+            public Vertex GetEndVertex()
             {
-                return endVertexNumber;
+                return endVertex;
             }
-            public void SetStartVertexNumber(int startVertexNumber)
+            public void SetStartVertexNumber(Vertex startVertex)
             {
-                this.startVertexNumber = startVertexNumber;
+                this.startVertex = startVertex;
             }
-            public void SetEndVertexNumber(int endVertexNumber)
+            public void SetEndVertexNumber(Vertex endVertex)
             {
-                this.endVertexNumber = endVertexNumber;
+                this.endVertex = endVertex;
             }
             public void SetModifier(float modifier)
             {
@@ -129,49 +130,52 @@ namespace GraphsComputerNetwork
                 return isDirected;
             }
         }
-        
+
         private Vertex[] vertices;
-        private Branch[] branches;
+        private Edge[] edges;
         private float[][] adjacencyMatrix;
+        private string name;
         public Graphs() {
             vertices = new Vertex[0];
-            branches = new Branch[0];
+            edges = new Edge[0];
         }
-        public Graphs(Vertex[] vertices, Branch[] branches)
+        public Graphs(Vertex[] vertices, Edge[] edges= null, string name = "")
         {
             this.vertices = vertices;
-            this.branches = branches;
-            //Дописать алгоритм заполнения матрицы расстояний
+            this.edges = edges;
+            this.name = name;
             adjacencyMatrix = new float[vertices.Length][];
             for (int i = 0; i < vertices.Length; i++)
             {
-                adjacencyMatrix[i]= new float[vertices.Length];
+                adjacencyMatrix[i] = new float[vertices.Length];
                 for (int j = 0; j < vertices.Length; j++)
                 {
                     adjacencyMatrix[i][j] = 0;
                 }
             }
-            foreach(Branch i in branches)
+            foreach (Edge i in edges)
             {
-                adjacencyMatrix[i.GetStartVertexNumber()][i.GetEndVertexNumber()] = i.GetModifier();
+                adjacencyMatrix[i.GetStartVertex().GetNumber()][i.GetEndVertex().GetNumber()] = i.GetModifier();
                 if (!i.GetDirection())
                 {
-                    adjacencyMatrix[i.GetEndVertexNumber()][i.GetStartVertexNumber()] = i.GetModifier();
+                    adjacencyMatrix[i.GetEndVertex().GetNumber()][i.GetStartVertex().GetNumber()] = i.GetModifier();
                 }
             }
+
+            this.name = name;
         }
-        public void AddBranch(Vertex Vertex1, Vertex Vertex2, float modifier,bool isDirected)
+        public void AddEdge(Vertex Vertex1, Vertex Vertex2, float modifier, bool isDirected)
         {
-            branches.Append(new Branch(isDirected,Vertex1.GetNumber(),Vertex2.GetNumber(),branches.Length,Vertex1.GetDistance(Vertex2),modifier));
+            edges.Append(new Edge(isDirected, Vertex1, Vertex2, edges.Length, Vertex1.GetDistance(Vertex2), modifier));
         }
-        public float[][] GetDistanceMatrix()
+        public float[][] GetAdjacencyMatrix()
         {
             return adjacencyMatrix;
         }
         public float[] GetAdjecencyMatrix(int numberOfPoint)
         {
             return adjacencyMatrix[numberOfPoint];
-        } 
+        }
         public void AddVertex(Vertex newVertex)
         {
             newVertex.SetNumber(vertices.Length);
@@ -185,29 +189,37 @@ namespace GraphsComputerNetwork
         {
             return vertices[number];
         }
-        public Branch[] GetBranches()
+        public Edge[] GetEdges()
         {
-            return branches;
+            return edges;
         }
-        public Branch GetBranches(int number)
+        public Edge GetEdges(int number)
         {
-            return branches[number];               
+            return edges[number];
         }
         public void SetVertexCoordinates(int PointNumber, int x, int y)
         {
             vertices[PointNumber].SetCoordinates(x, y);
         }
-        public IEnumerable<int> GetAdjacentVertices(int vertexNumber)
+        public IEnumerable<Vertex> GetAdjacentVertices(int vertexNumber)
         {
             if (vertexNumber < 0 || vertexNumber >= this.vertices.Length) throw new ArgumentOutOfRangeException("Cannot access vertex");
 
-            List<int> adjacentVertices = new List<int>();
+            List<Vertex> adjacentVertices = new List<Vertex>();
             for (int i = 0; i < this.vertices.Length; i++)
             {
                 if (this.adjacencyMatrix[vertexNumber][i] > 0)
-                    adjacentVertices.Add(i);
+                    adjacentVertices.Add(vertices[i]);
             }
             return adjacentVertices;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
         }
     }
 }
